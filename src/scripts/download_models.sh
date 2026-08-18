3354ab2d5733740a4ffd41bcdcda4c3ca2ef1c30
k19tvan <24520073@gm.uit.edu.vn>
2026-08-18 03:31:59 +0000

    Clean repo

--- src/scripts/download_models.sh
+++ src/scripts/download_models.sh
@@ -1,3 +1,4 @@
 hf download BAAI/BGE-VL-large --local-dir models/BGE-VL-large
 hf download facebook/metaclip-2-worldwide-huge-quickgelu --local-dir models/metaclip-2-worldwide-huge-quickgelu
 hf download llnh/beit3_large_patch16_384_coco_retrieval --local-dir models/beit3_large_patch16_384_coco_retrieval
+hf download qihoo360/fg-clip2-large --revision 4d1d5dc35c716902f07c172dbfc23b82a7bc6bf3 --local-dir models/fg-clip2-large
